d1274055cbe5ec2b0729b613da9f8d65c698cabe
Rasmus Lohals <rll@schultz.dk>
2018-10-01 23:23:24 +0200

    sumof mulitples

---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,8 @@
+﻿module SumOfMultiples
+
+let isMultipleOf numberToExamine list = List.exists (fun number -> numberToExamine % number = 0) list
+
+let sum (numbers: int list) (upperBound: int): int = 
+    [0..upperBound-1]
+    |> List.filter (fun n -> isMultipleOf n numbers)
+    |> List.sum
