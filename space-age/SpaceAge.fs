99bc0c067bd198633afd701d35f203de304de4a4
Rasmus Lohals <rll@schultz.dk>
2018-10-06 10:04:00 +0200

    spaceage

---
+++ space-age/SpaceAge.fs
@@ -0,0 +1,21 @@
+﻿module SpaceAge
+
+type Planet = Earth|Mercury|Venus|Mars|Jupiter|Saturn|Uranus|Neptune
+
+let secondsPerEarthYear = 31557600.0
+
+let earthYear seconds orbitalPeriodFactor = 
+    let orbitalPeriodSeconds = orbitalPeriodFactor * secondsPerEarthYear
+    (float seconds) / orbitalPeriodSeconds
+
+let age (planet: Planet) (seconds: int64): float = 
+    match planet with
+    | Earth -> 1.0
+    | Mercury -> 0.2408467
+    | Venus -> 0.61519726
+    | Mars -> 1.8808158
+    | Jupiter -> 11.862615
+    | Saturn -> 29.447498
+    | Uranus -> 84.016846
+    | Neptune -> 164.79132
+    |> earthYear seconds
