6c5874c8fe0688a79a64869905352026b9a37693
Rasmus Lohals <rll@schultz.dk>
2018-10-11 23:14:42 +0200

    raindrops

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,21 @@
+﻿module Raindrops
+
+let vocabulary = [
+    (3,"Pling");
+    (5,"Plang");
+    (7,"Plong")
+    ]
+let hasFactor factor number = (number % factor = 0)
+
+let convertToKnownString (factor, output) number = 
+    if hasFactor factor number 
+    then output 
+    else ""
+
+let convert (number: int): string = 
+    vocabulary
+    |> List.map (fun vocab -> convertToKnownString vocab number)
+    |> List.reduce (+) 
+    |> function
+        | "" -> number.ToString()
+        | output -> output
